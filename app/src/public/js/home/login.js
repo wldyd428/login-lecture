791fd66221007cf42a31caa14c483289df280b5e
강지용 <yong@gangjiyong-ui-MacBookPro.local>
2023-02-16 16:15:06 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello");
-console.log("bye")
+//DOM = Document Object Model
+//문서 객체 모델 : 일종의 인터페이스
+//자바스크립트에서 html에 존재하는 데이터들을 가져와서 제어할 수 있게 됨
+
+const id = document.querySelector("#id"),
+ pasword = document.querySelector("#psword"),
+ loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pasword: pasword.value,
+    };
+};
