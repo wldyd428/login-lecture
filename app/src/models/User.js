00455bce3a04ca904117fd92ef39b9ab7ede1887
강지용 <yong@gangjiyong-ui-MacBookPro.local>
2023-02-21 19:58:54 +0900

    AWS RDS(나는EC2)와 연동 & 환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,19 @@ class User {
 
     async login() { //await은 async 안에서만 사용할 수 있음
         const client = this.body;
-        const { id, psword } = await UserStorage.getUserInfo(client.id); //await은 항상 Promise를 반환하는 애한테만 해줄 수 있음, Promise를 반환하기 때문에 .then()으로도 접근하여 데이터를 가져올 수 있음. await을 사용해준 이유는 "가독성"때문. fs(파일시스템)에서도 await으로 가져올 수 있음. 본인의 개발 성향에 맞춰서 하면 됨.
-        
-        if (id) {
-            if (id === client.id && psword === client.psword) {
-                return { success: true };
-            }
-            return { success: false, msg: "비밀번호가 틀렸습니다."};
+        try {
+          const { id, psword } = await UserStorage.getUserInfo(client.id); //await은 항상 Promise를 반환하는 애한테만 해줄 수 있음, Promise를 반환하기 때문에 .then()으로도 접근하여 데이터를 가져올 수 있음. await을 사용해준 이유는 "가독성"때문. fs(파일시스템)에서도 await으로 가져올 수 있음. 본인의 개발 성향에 맞춰서 하면 됨.
+          
+          if (id) {
+              if (id === client.id && psword === client.psword) {
+                  return { success: true };
+              }
+              return { success: false, msg: "비밀번호가 틀렸습니다."};
+          }
+          return { success: false, msg: "존재하지 않는 아이디입니다."};
+        } catch (err) {
+          return { success: false, msg: err}
         }
-        return { success: false, msg: "존재하지 않는 아이디입니다."};
     }
 
     async register() {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,66 +1,29 @@
 "use strict"
 
-const fs = require("fs").promises; //Promise는 약속이라는 의미로 Promise가 수행하는 동작이 끝남과 동시에 상태를 알려주기 때문에 비동기 처리에 아주 효과적
+const db = require("../config/db");
 
 class UserStorage {
-  static #getUserInfo(data, id) {
-    const users = JSON.parse(data);
-    const idx = users.id.indexOf(id);
-    const userKeys = Object.keys(users); // => [id, psword, name]
-    const userInfo = userKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
-  }
-  
-  static #getUsers(data, isAll, fields) {
-    const users = JSON.parse(data);
-    if (isAll) return users;
-
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
-
-  static getUsers(isAll, ...fields) {
-    return fs
-      .readFile("./src/databases/users.json")
-      .then((data) => {
-        return this.#getUsers(data, isAll, fields);
-      })
-      .catch(console.error);
-  }
-
   static getUserInfo(id) {
-    return fs
-      .readFile("./src/databases/users.json")
-      //.then() //Promise를 반환하게 되면 then이라는 메소드에 접근 가능, 해당 로직이 성공했을 때 실행됨
-      //.catch(); //Promise를 반환하는 것에 대한 오류처리는 catch로 해줄 수 있음, 해당 로직이 실패했을 때 실행됨
-      .then((data) => {
-        return this.#getUserInfo(data, id);
-      })
-    //.catch((err) => console.error(err)); //함수를 실행 시키는데 파라미터로 넘어오는 변수를 실행시키는 함수로 똑같이 넘기게 되면 생략 가능
-      .catch(console.error);
-  }
-
-
+    return new Promise((resolve, reject) => { //promise 안에 있는 구문이 성공을 하면 reserve를 실행, 실패 시 reject를 실행
+      const query = "SELECT * FROM users WHERE id = ?;";
+      db.query(query, [id], (err, data) => {
+        if(err) reject(`${err}`);
+        resolve(data[0]);
+      });
+    })
+  };
 
   static async save(userInfo) {
-    const users = await this.getUsers(true);
-    if (users.id.includes(userInfo.id)) {
-      throw "이미 존재하는 아이디입니다.";
-    }
-    users.id.push(userInfo.id);
-    users.name.push(userInfo.name);
-    users.psword.push(userInfo.psword);
-    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
-    return { success: true };
-  
+    return new Promise((resolve, reject) => { //promise 안에 있는 구문이 성공을 하면 reserve를 실행, 실패 시 reject를 실행
+      const query = "INSERT INTO users(id, name, psword) value(?, ?, ?);";
+      db.query(
+        query, 
+        [userInfo.id, userInfo.name, userInfo.psword], 
+        (err) => {
+        if(err) reject(`${err}`);
+        resolve({ success: true });
+      });
+    })
   }
 }
 
